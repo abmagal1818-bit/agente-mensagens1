12098f27d111a5c073714f53b931d02554b0b405
abmagal1818-bit <abmagal1818@gmail.com>
2026-05-27 16:33:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -46,5 +46,5 @@ app.post("/webhook", async (req, res) => {
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
