f362f7d6d77e34e96b9596f2e9733a60a1d4cab8
abmagal1818-bit <abmagal1818@gmail.com>
2026-06-12 13:04:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const FormData = require("form-data");
 const https = require("https");
-const app = express()
+const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = "meu_token_verificacao";
