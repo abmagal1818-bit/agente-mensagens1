9791141a0df28e288037c165fcfc58b37986c2de
abmagal1818-bit <abmagal1818@gmail.com>
2026-06-16 06:57:44 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,4 @@
 const CACHE_NAME = "sarah-crm-v1";
-const urlsToCache = ["/painel"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(["/painel"]))));
+self.addEventListener("fetch", e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))));
